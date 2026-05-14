25e8021c5ce7cb97b655f80a9f92fea92913df0e
Eric Henrique <154543763+eric-less@users.noreply.github.com>
2026-05-13 21:14:36 -0300

    Delete src directory

--- src/main/java/br/unicamp/padroescriacionais/legacy/domain/FormatoRelatorio.java
+++
@@ -1,7 +0,0 @@
-package br.unicamp.padroescriacionais.legacy.domain;
-
-public enum FormatoRelatorio {
-    PDF,
-    CSV,
-    JSON
-}
--- src/main/java/br/unicamp/padroescriacionais/legacy/domain/TipoRelatorio.java
+++
@@ -1,7 +0,0 @@
-package br.unicamp.padroescriacionais.legacy.domain;
-
-public enum TipoRelatorio {
-    VENDAS,
-    ESTOQUE,
-    CLIENTES
-}
